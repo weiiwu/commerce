dba510049f499b3e05797a668830cd283c02868b
weiiwu <master_wu51@hotmail.com>
2025-06-25 15:14:57 +1000

    fix params

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -4,9 +4,10 @@ import { stripe } from "@/lib/stripe";
 export default async function ProductPage({
     params,
 }:{
-    params: {id: string};
+    params: Promise<{id: string}>;
 }) {
-    const product = await stripe.products.retrieve(params.id, {
+    const { id } = await params;
+    const product = await stripe.products.retrieve(id, {
         expand: ["default_price"],
     });
 
